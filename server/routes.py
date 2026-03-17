43bfbf04c104a703cfba67be5e23e845d32a1367
joung-suh <124483145+joung-suh@users.noreply.github.com>
2026-03-17 15:33:51 +0900

    Refactor SQL queries in routes.py
    Fix SQL query formatting and remove redundant lines.

--- server/routes.py
+++ server/routes.py
@@ -13,7 +13,7 @@ def index():
 
     if name:
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE %s" + name +  "%s"
+            "SELECT * FROM books WHERE name LIKE %s", name
         )
         books = [Book(*row) for row in cursor]
 
@@ -21,8 +21,7 @@ def index():
         cursor.execute(
             "SELECT * FROM books WHERE author LIKE %s", author
 
-        )
-        books = [Book(*row) for row in cursor]
+        )        books = [Book(*row) for row in cursor]
 
     else:
         cursor.execute("SELECT name, author, read FROM books")
